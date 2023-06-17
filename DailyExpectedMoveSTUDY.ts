f2d05acda002c2a20f02191a648b26a67a2089b4
Benjamin Johnson <benjaminkylejohnson@gmail.com>
2023-06-17 17:32:09 +1000

    Update DailyExpectedMoveSTUDY.ts for

--- DailyExpectedMoveSTUDY.ts
+++ DailyExpectedMoveSTUDY.ts
@@ -1,6 +1,6 @@
 # Daily Expected Move
 # Author: Benny33
-# Date: 06-16-2023
+# Date: 06-17-2023
 #
 
 input showDailyEM = yes;
@@ -86,6 +86,7 @@ def spxExpectedMove =
     else if date == 20230614 then 34.896
     else if date == 20230615 then 26.1
     else if date == 20230616 then 26.782
+    else if date == 20230620 then 26.272
 else nil;
 
 def spyExpectedMove =
@@ -157,6 +158,7 @@ def spyExpectedMove =
     else if date == 20230614 then 3.83
     else if date == 20230615 then 3.002
     else if date == 20230616 then 3.499
+    else if date == 20230620 then 2.732
 else nil;
 
 def qqqExpectedMove =
@@ -228,6 +230,7 @@ def qqqExpectedMove =
     else if date == 20230614 then 4.791
     else if date == 20230615 then 3.938
     else if date == 20230616 then 4.42
+    else if date == 20230620 then 3.301
 else nil;
 
 def ndxExpectedMove =
